5c7f46c06b49bb1622ee495a7143163204f1ea08
Vichigato-yt <vicejo1009@gmail.com>
2026-01-14 01:26:07 +0000

    ✨ feat(sensors): crear servicio de conexión con acelerómetro
    Implementa SensorService con métodos subscribe/unsubscribe para gestionar listeners del hardware MEMS"

--- lib/modules/sensors/accelerometer/accelerometer.service.ts
+++ lib/modules/sensors/accelerometer/accelerometer.service.ts
@@ -1,1 +1,18 @@
 // Conexión Directa
+import { ACCEL_UPDATE_INTERVAL_MS } from "@/lib/core/logic/constants";
+import { Accelerometer } from "expo-sensors";
+
+export type AccelerometerData = { x: number; y: number; z: number };
+
+export const SensorService = {
+	// Método para iniciar la escucha del hardware
+	subscribe: (callback: (data: AccelerometerData) => void) => {
+		Accelerometer.setUpdateInterval(ACCEL_UPDATE_INTERVAL_MS);
+		return Accelerometer.addListener(callback);
+	},
+
+	// Limpieza para evitar memory leaks
+	unsubscribe: (subscription: { remove: () => void } | null | undefined) => {
+		if (subscription) subscription.remove();
+	},
+};
